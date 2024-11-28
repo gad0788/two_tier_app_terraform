6eb93972176bc8e655130d53cbc7b8bc6a0786f0
gad0788 <156004978+gad0788@users.noreply.github.com>
2024-11-27 23:21:15 -0700

    Update backend.tf

--- 10WeeksOfCloudOps_Task3/root/backend.tf
+++ 10WeeksOfCloudOps_Task3/root/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "remote-gn-terrraform"
+    bucket         = ""
     key            = "two_tier_app/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "remote-state-lock"
+    dynamodb_table = ""
   }
 }
